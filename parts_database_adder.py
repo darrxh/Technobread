0a5ee588dc897be4128b5dab23218f7f28a57488
DivoHub <39216431+DivoHub@users.noreply.github.com>
2021-09-03 19:59:36 -0700

    quick

--- parts_database_adder.py
+++ parts_database_adder.py
@@ -1,19 +1,25 @@
-class Ram:
-    def __init__(self):
-        self.brand = "N/A"
-        self.size ="N/A"
-        self.speed ="N/A"
+
 
 class Cpu:
     def __init__(self):
+        #Base info variables
         self.brand ="N/A"
         self.model ="N/A"
-        self.rank ="N/A"
-        self.benchmark ="N/A"
         self.cores ="N/A"
-        self.thread ="N/A"
-        self.clock ="N/A"
-        self.release ="N/A"
+        self.threads ="N/A"
+        self.base_clock ="N/A"
+        self.boost_clock = "N/A"
+        #Extra info variables
+        self.release_date ="N/A"
+        self.benchmark_percentage ="N/A"
+        self.rank ="N/A"
+        self.integrated_graphics = "N/A"
+        self.url = "N/A"
+        self.price = "N/A"
+
+    def add_cpu(self):
+
+
 
 class Gpu:
     def __init__(self):
@@ -22,4 +28,20 @@ class Gpu:
         self.rank ="N/A"
         self.benchmark ="N/A"
         self.vram ="N/A"
+
+
+class Ram:
+    def __init__(self):
+        self.brand = "N/A"
+        self.size ="N/A"
+        self.speed ="N/A"
+
+def main():
+    try:
+    input(print("Which part to add? CPU | GPU | RAM | MOBO | SSD | HDD | PSU | CASE"))
+    except N
+
+
+
+
 
