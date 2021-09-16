badb264f2f22f7503be0659877ef93e42060e51e
DivoHub <39216431+DivoHub@users.noreply.github.com>
2021-09-16 13:14:13 -0700

    Trash code

--- parts_database_updater.py
+++ parts_database_updater.py
@@ -19,7 +19,7 @@ def pcpp_data_update():
         part_object = api.retrieve(part)
         print ("Part object retrieved... \n creating JSON text")
         json_text = part_object.to_json()
-        with open("PCPP_" + part + ".json","w") as database:
+        with open("Data/" + part + "_Data_PCPP.json","w") as database:
             print ("Creating/Writing text file...")
             database.write(json_text + "\n")
             print ("Write successful")
@@ -32,9 +32,48 @@ def ub_data_update():
 
 def create_json():
 
-    
+    print ("coming soon")
+
+def common_part_counter():
+
+    with open("Data/cpu_Data_PCPP.json","r") as pcpp_data:
+        json_string_pcpp = pcpp_data.read()
+        data_list_pcpp = json.loads(json_string_pcpp)
+    with open("Data/cpu_Data_UB.json","r") as ub_data:
+        json_string_ub = ub_data.read()
+        data_list_ub = json.loads(json_string_ub)
+
+    print ("Number of elements in UB: " + str(len(data_list_ub)))
+    print ("Number of elements in PCPP: " + str(len(data_list_pcpp)))
 
 
+
+
+    counter = 0
+    common_list = []
+    uncommon_list = []
+
+    for pcpp_index in data_list_pcpp:
+        condition = False
+        for ub_index in data_list_ub:
+            if (ub_index['Model'] == pcpp_index['model']):
+                counter = counter + 1
+                common_list.append(ub_index['Model'])
+                condition = True
+        if (not(condition)):
+            uncommon_element = pcpp_index['model']
+            uncommon_list.append(uncommon_element)
+
+    print ("Elements in common: " + str(counter))
+    with open("Data/common_cpu_list.txt","w") as finish_data:
+        for part in common_list:
+            finish_data.write(part + "\n")
+
+    with open("Data/uncommon_cpu_list.txt","w") as uncommon_data:
+        for uncommon in uncommon_list:
+            uncommon_data.write(uncommon + "\n")
+
+common_part_counter()
 
 
 
