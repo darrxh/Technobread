c725a9feb956272ceeb7953ec58396b51e906cdd
DivoHub <39216431+DivoHub@users.noreply.github.com>
2021-09-16 14:47:31 -0700

    BRUH

--- parts_database_updater.py
+++ parts_database_updater.py
@@ -43,36 +43,64 @@ def common_part_counter():
         json_string_ub = ub_data.read()
         data_list_ub = json.loads(json_string_ub)
 
-    #move these print states v
     print ("Number of elements in UB: " + str(len(data_list_ub)))
     print ("Number of elements in PCPP: " + str(len(data_list_pcpp)))
 
     ub_list = []
     pcpp_list = []
     common_list = []
-    counter = 0
 
+    ub_remainders_list = []
+    pcpp_remainders_list = []
 
     for pcpp_index in data_list_pcpp:
-        for ub_index in data_list_ub:
-            if (ub_index['Model'] == pcpp_index['model']):
-                counter = counter + 1
-                common_list.append(ub_index['Model'])
-
-
-    for pcpp_index in (0,len(data_list_pcpp)):
-        for common_index in common_list:
-            if (data_list_pcpp[pcpp_index]['model'] == common_index):
-
+        pcpp_list.append(str(pcpp_index['model']))
     for ub_index in data_list_ub:
-        for common_index in common_list:
-            if (ub_index['Model'] == common_index):
+        ub_list.append(str(ub_index['Model']))
+    ub_list.sort()
+    pcpp_list.sort()
 
+    for ub_index in ub_list:
+        for pcpp_index in pcpp_list:
+            if (ub_index == pcpp_index):
+                common_list.append(str(ub_index))
 
-    print ("Elements in common: " + str(counter))
-    with open("Data/common_cpu_list.txt","w") as finish_data:
+    print ("Elements in common: " + str(len(common_list)))
+
+    with open("Data/common_cpu_list.txt","w") as common_cpu_list:
         for part in common_list:
-            finish_data.write(part + "\n")
+            common_cpu_list.write(str(part) + "\n")
+
+
+    for pcpp_index in pcpp_list:
+        is_different = True
+        for common_index in common_list:
+            if (common_index == pcpp_index):
+                is_different = False
+        if (is_different):
+            pcpp_remainders_list.append(pcpp_index)
+
+
+    for ub_index in ub_list:
+        is_different = True
+        for common_index in common_list:
+            if (common_index == ub_index):
+                is_different = False
+        if (is_different):
+            ub_remainders_list.append(ub_index)
+
+
+    print ("Remaining elements in UB file:  " + str(len(ub_remainders_list)))
+    with open ("Data/remainders_ub.txt", "w") as remainders_ub:
+        for part in ub_remainders_list:
+            remainders_ub.write(str(part) + "\n")
+
+    print ("Remaining elements in PCPP file: " + str(len(pcpp_remainders_list)))
+    with open("Data/remainders_pcpp.txt", "w") as remainders_pcpp:
+        for part in pcpp_remainders_list:
+            remainders_pcpp.write(str(part) + "\n")
+
+
 
 
 
